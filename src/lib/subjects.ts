04e37537eb66012b718ad2a0e6b42212f51ede96
PANKAJA D.L.K <pankajadlk.23@uom.lk>
2025-06-16 17:00:44 +0530

    Add boolean algebra

--- src/lib/subjects.ts
+++ src/lib/subjects.ts
@@ -1,9 +1,11 @@
 import {pc } from '../data/pc';
 import { dsa } from '../data/dsa';
 import { codd } from '../data/codd';
+import { boolean } from '../data/boolean';
 
 export const subjects = new Map<string, { name: string, questions: any[] }>([
     ['pc', { name: 'Program Construction', questions: pc }],
     ['dsa', { name: 'Data Structures and Algorithms', questions: dsa }],
-    ['codd', { name: 'Computer Organization and Digital Design', questions: codd }]
+    ['codd', { name: 'Computer Organization and Digital Design', questions: codd }],
+    ['bool', { name: 'Boolean Algebra', questions: boolean }]
 ]);
